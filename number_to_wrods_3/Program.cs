a8e83443a2ea21941077139adb6a4c948b2506d7
Rohanbellindependentsolutions <rohan.zane-bell@independentsolutions.com.au>
2022-09-23 11:08:41 +1000

    Updated
    Adding comments and double checking for any numbers that do not fit

--- number_to_wrods_3/Program.cs
+++ number_to_wrods_3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Threading;
 
 namespace number_to_words_2
 {
@@ -16,32 +17,36 @@ namespace number_to_words_2
          * 
          */
         {
-            Console.WriteLine("Please Enter Your Designated Number:");
+            Console.WriteLine("Please Enter Your Designated Number between zero and ten million:");
             int input = Convert.ToInt32(Console.ReadLine());
             int answer, next, num_digits, digits_num, count = 0, count_num;
             int[] a = new int[10];
 
             // words for every digits from 0 to 9
             string[] digits_words = {
-            "zero",
-            "one",
-            "two",
-            "three",
-            "four",
-            "five",
-            "six",
-            "seven",
-            "eight",
-            "nine",
-            "ten",
-            "eleven",
-            "twelve",
-            "twenty",
-            "thirty",
+            "Zero",
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine",
+            "Ten",
+            "Eleven",
+            "Twelve",
+            "Twenty",
+            "Thirty",
+            "fourty",
+            "fifty",
+            "sixty",
+            "seventy",
+            "eighty",
+            "ninety",
             "teen",
-            "ty",
-            "hundred",
-            "thousand"
+            "ty"
          };
 
             answer = input;
@@ -51,44 +56,195 @@ namespace number_to_words_2
                 count_num = count_num / 10;
                 count++;
             }
-            //Console.WriteLine("Number of digits is : " + count);
-            //Console.ReadLine();
-            while(count > 0)
-            {
-                if (count == 5)
-                {
-                    Console.Write(digits_words[answer / 1000] + " Ten Thousand ");
-                }
-                if (count == 4)
-                {
-                    Console.Write(digits_words[answer / 1000] + " Thousand ");
-                }
+            bool million = false;
+            bool hthousand = false;
+            bool tthousand = false;
+            bool thousand = false;
+            bool hundred = false;
+            bool teens = false;
+            bool ones = false;
 
-                if (count == 3)
+            if (count > 7 || answer < 0)
+                //Check if number is greater then a million and not negative
+            {
+                Console.Write("This is not a valid number");
+            }
+            else
+            {
+
+                while (count >= 0)
+                    //Check to see if there are any numbers left in the answer
                 {
-                    Console.Write(digits_words[answer / 100] + " hundred ");
-                }
-                if (count == 2)
-                {
-                    if (answer / 10 < 4)
+
+                    if (count == 7)
                     {
-                        Console.Write(digits_words[answer/10 + 11] +'-');
+                        //If a number is a million or more
+                        Console.Write(digits_words[answer / 1000000] + " Million ");
+                        //Removes the millionth digit
+                        answer = answer % 1000000;
+                        million = true;
                     }
                     else
                     {
-                        Console.Write(digits_words[answer / 10] + "ty-");
+                        if (count == 6)
+                        {
+                            if (answer / 100000 == 0){
+
+
+                            }
+                            else
+                            {
+                                Console.WriteLine(digits_words[answer / 100000] + " Hundred");
+                                answer = answer % 100000;
+                                hthousand = true;
+                            }
+                        }
+                        else
+                        {
+                            if (count == 5)
+                            {
+                                if (answer / 10000 == 0)
+                                {
+
+                                }
+                                else
+                                {
+                                    if (hthousand)
+                                    {
+                                        Console.WriteLine(" and");
+                                    }
+                                    if (answer / 10000 == 1)
+                                    {
+                                        Console.WriteLine("Ten");
+                                        answer = answer % 10000;
+                                        tthousand = true;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine(digits_words[answer / 10000 + 11]);
+                                        answer = answer % 10000;
+                                        tthousand = true;
+                                    }
+                                };
+                            }
+                            else
+                            {
+                                if (count == 4)
+                                {
+                                    if (answer / 1000 == 0)
+                                    {
+                                        if (hthousand || tthousand){
+                                            Console.WriteLine(" Thousand");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("-" + digits_words[answer / 1000] + " Thousand");
+                                        answer = answer % 1000;
+                                        thousand = true;
+                                    }
+
+                                }
+                                else
+                                {
+                                    if (count == 3)
+                                    {
+                                        if (answer / 100 == 0)
+                                        {}
+                                        else
+                                        {
+                                            Console.WriteLine(digits_words[answer / 100] + " Hundred");
+                                            answer = answer % 100;
+                                            hundred = true;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if (count == 2)
+                                        {
+                                            if (answer / 10 == 0)
+                                            {
+
+                                            }
+                                            else
+                                            {
+                                                if (hundred){
+                                                    Console.Write("and");
+                                                }
+                                                if (answer / 10 == 1)
+                                                {
+                                                    teens = true;
+                                                    answer = answer % 10;
+                                                }
+                                                else
+                                                {
+                                                    Console.WriteLine(digits_words[answer / 10 + 11] + "");
+                                                    answer = answer % 10;
+                                                    
+                                                }
+                                            }
+                                        }
+                                        else
+                                        {
+                                            if (count == 1)
+                                            {
+                                                if(answer == 0)
+                                                {
+                                                    if (teens)
+                                                    {
+                                                        Console.Write("Ten");
+                                                    }
+                                                }
+                                                else
+                                                {
+                                                    if (teens)
+                                                    {
+                                                        if (answer < 3 || answer == 5)
+                                                            //11, 12, 15
+                                                        {
+                                                            if (answer == 5)
+                                                                //15
+                                                            {
+                                                                Console.Write("Fifteen");
+                                                            }
+                                                            else
+                                                            {
+                                                                //Eleven or Twelve
+                                                                Console.WriteLine(digits_words[answer + 10]);
+                                                            }
+                                                        }
+                                                        else
+                                                        {
+                                                            //Thirteen to 19
+                                                            Console.WriteLine(digits_words[answer] + "teen");
+
+                                                        }
+                                                    }
+                                                    else
+                                                    {
+                                                        //Twenty and above or below 10
+                                                        Console.WriteLine("-" + digits_words[answer] + "");
+                                                        answer = answer % 1;
+                                                        ones = true;
+                                                    }
+                                                }
+                                            }
+                                        }
+
+                                    }
+                                }
+                            }
+
+                        }
                     }
-                }
-                if (count == 1)
-                {
-                    Console.Write(digits_words[answer%10 + 1]);
-                }
+                    count--;
 
 
-                answer = answer / 10;
-                count--;
+
+
+
+                }
             }
-
         }
     }
 }
